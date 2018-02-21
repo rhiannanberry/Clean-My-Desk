f5fadf101725373d0a0bf5cfa760651a0bb87b3d
rhiannanberry <rhiannanb97@gmail.com>
2018-02-21 01:33:41 -0500

    Basic start option menu. Can be modified for use in pause menu

--- Assets/Scripts/Managers/MenuManager.cs
+++ Assets/Scripts/Managers/MenuManager.cs
@@ -2,12 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuManager : MonoBehaviour {
-
+	public bool startMenu = true;
+	private Canvas monitor1 = null;
+	private Canvas monitor2 = null;
 	// Use this for initialization
 	void Start () {
-		
+		if (startMenu) {
+			startMenuSetUp();
+		}	
 	}
 	
 	// Update is called once per frame
@@ -17,5 +22,45 @@ public class MenuManager : MonoBehaviour {
 
 	public void GotoSceneIndex(int i) {
 		SceneManager.LoadScene(i);
+	}
+
+	public void OpenOptions() {
+		if (startMenu) {
+			monitor1.enabled = false;
+			monitor2.enabled = true;
+		}
+	}
+
+	public void ExitOptions() {
+		if (startMenu) {
+			monitor1.enabled = true;
+			monitor2.enabled = false;
+		}
+	}
+
+	//Save and cancel options shouldn't need specific code for start vs pause
+	public void CancelOptions() {
+
+	}
+
+	public void SaveOptions() {
+
+	}
+
+	public void Exit() {
+		if (startMenu) {
+			//should add exit animation probably
+			#if UNITY_EDITOR
+				UnityEditor.EditorApplication.isPlaying = false;
+			#else
+				Application.Quit();
+			#endif
+		}
+	}
+
+	private void startMenuSetUp() {
+		monitor1 = GameObject.Find("Monitor1/Canvas").GetComponent<Canvas>();
+		monitor2 = GameObject.Find("Monitor2/Canvas").GetComponent<Canvas>();
+		monitor2.enabled = false;
 	}
 }
