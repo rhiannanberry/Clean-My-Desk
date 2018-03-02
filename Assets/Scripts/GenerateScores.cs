9cac53b44482e6c7909e19fcb945199d0fa664c0
rhiannanberry <rhiannanb97@gmail.com>
2018-03-02 16:38:58 -0500

    Added fonts, more ui things, and basic random score generation

---
+++ Assets/Scripts/GenerateScores.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GenerateScores : MonoBehaviour {
+	public GameObject scoreItemPrefab;
+
+	public List<string> scoreDesc;
+	public int length = 3;
+
+	private List<string> notListed;
+	// Use this for initialization
+	void Start () {
+		notListed = new List<string>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void SpawnScoreItems() {
+		for (int i = 0; i < length; i ++) {
+			GameObject scr = Instantiate(scoreItemPrefab);
+			scr.transform.SetParent(transform);
+			scr.transform.localScale = new Vector3(1,1,1);
+			string str = scoreDesc[Random.Range(0,scoreDesc.Count)];
+			scr.GetComponentInChildren<Text>().text = str + ": " + Random.Range(1,99999).ToString();
+			scoreDesc.Remove(str);
+		}
+	}
+}
