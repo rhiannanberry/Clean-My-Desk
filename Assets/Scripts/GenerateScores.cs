e4640cfde3511f92523f003dba0165df2fc9ccb2
rhiannanberry <rhiannanb97@gmail.com>
2018-03-15 02:13:45 -0400

    Animation for transition between initial and actual countdown for timed mode added. Game loop closed for timed mode!! item select made inaccessible in timed mode. More stuff moved to text mesh pro

--- Assets/Scripts/GenerateScores.cs
+++ Assets/Scripts/GenerateScores.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
 
 public class GenerateScores : MonoBehaviour {
 	public GameObject scoreItemPrefab;
@@ -11,6 +13,7 @@ public class GenerateScores : MonoBehaviour {
 
 	private List<string> notListed;
 	// Use this for initialization
+
 	void Start () {
 		notListed = new List<string>();
 	}
@@ -22,19 +25,27 @@ public class GenerateScores : MonoBehaviour {
 
 	public void SpawnScoreItems() {
 		int tot = 0;
+		List<string> listHold = new List<string>();
+		float parentHeight = GetComponent<RectTransform>().rect.height;
+		float childHeight = scoreItemPrefab.GetComponent<RectTransform>().rect.height;
+		int length = (int) Mathf.Floor(parentHeight / childHeight);
 		for (int i = 0; i < length; i ++) {
 			GameObject scr = Instantiate(scoreItemPrefab);
 			scr.transform.SetParent(transform);
 			scr.transform.localScale = new Vector3(1,1,1);
 			string str = scoreDesc[Random.Range(0,scoreDesc.Count)];
 			int scoreNum = Random.Range(1,9999);
-			scr.GetComponentInChildren<Text>().text = str + ": " + scoreNum.ToString();
+			scr.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = str;
+			scr.transform.GetChild(1).GetComponent<TextMeshProUGUI>().text = scoreNum.ToString();
+			listHold.Add(str);
 			scoreDesc.Remove(str);
 			tot += scoreNum;	
 		}
+			scoreDesc.AddRange(listHold);
 			GameObject totscr = Instantiate(scoreItemPrefab);
 			totscr.transform.SetParent(transform);
 			totscr.transform.localScale = new Vector3(1,1,1);
-			totscr.GetComponentInChildren<Text>().text = "Total:   " + tot.ToString();
+			totscr.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = "Total";
+			totscr.transform.GetChild(1).GetComponent<TextMeshProUGUI>().text = tot.ToString();
 	}
 }
--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -2,26 +2,59 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class Timer : MonoBehaviour {
 	public float time = 30;
-	private Text text;
+	public GameObject endMenu;
+	public GameObject itemsContainer;
+	private TextMeshProUGUI text;
+	private bool continueCount = true;
+	private bool start = false;
 	// Use this for initialization
 	void Start () {
-		text = transform.GetComponent<Text>();
+		endMenu.transform.Find("MainModeUI").gameObject.SetActive(false);
+		endMenu.transform.localScale = new Vector3(0.0f,1,1);
+		text = transform.GetComponent<TextMeshProUGUI>();
+		//text.text = time.ToString();
+		StartCoroutine(InitCountdown());
+	}
+
+	IEnumerator InitCountdown() {
+		text.text = "3";
+		yield return new WaitForSeconds(1);
+		text.text = "2";
+		yield return new WaitForSeconds(1);
+		text.text = "1";
+		yield return new WaitForSeconds(1);
+		text.text = "GO!";
+		GetComponent<Animator>().SetBool("Start", true);
+		yield return new WaitForSeconds(0.5f);
 		text.text = time.ToString();
+		start = true;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		time -= Time.deltaTime;
-		if (time < 0 ) {
-			time = 0;
-		}
-		if (time >= 10) {
-			text.text = (time.ToString()).Substring(0, 4);
-		} else {
-			text.text = ' ' + (time.ToString()).Substring(0, 3);
+		if (continueCount && start) {
+			if (itemsContainer.transform.childCount == 0) {
+				continueCount = false;
+				endMenu.transform.Find("TimeModeUI/TimeModeFail").gameObject.SetActive(false);
+				//trigger menu
+				endMenu.GetComponent<Menuing>().ToggleMenu();
+			} else if (time < 0) {
+				time = 0;
+				continueCount = false;
+				endMenu.transform.Find("TimeModeUI/TimeModeSuccess").gameObject.SetActive(false);
+				//trigger menu
+				endMenu.GetComponent<Menuing>().ToggleMenu();
+			}
+			if (time >= 10) {
+				text.text = (time.ToString()).Substring(0, 2);
+			} else {
+				text.text = (time.ToString()).Substring(0, 1);
+			}
+			time -= Time.deltaTime;
 		}
 	}
 }
