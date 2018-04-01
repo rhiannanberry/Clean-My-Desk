cf67698650c5d790a62add66862fc15d58c7a387
rhiannanberry <rhiannanb97@gmail.com>
2018-04-01 13:38:38 -0400

    NAAA NA NA NA NA NA NA KATAMARI DAMACYYYYY

---
+++ Assets/Scripts/Test/BallController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour {
+	public Transform parentCamera;
+	private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float fwd = Input.GetAxis("Vertical") * 1.0f;
+		float horiz = Input.GetAxis("Horizontal") * 1.0f;
+		Debug.DrawRay(transform.position, transform.forward, Color.red);
+		Debug.DrawRay(parentCamera.position, parentCamera.forward, Color.red);
+		rb.AddTorque(parentCamera.right * fwd);
+		rb.AddTorque(parentCamera.forward * -horiz);
+
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.tag == "Item") {
+			Debug.Log("HEUGHGHH");
+			Transform item = GetItemRootParent(other.transform);
+			Destroy(item.GetComponent<Rigidbody>());
+			item.SetParent(transform);
+		}
+	}
+
+	Transform GetItemRootParent(Transform item) {
+		if (item.parent != null && item.parent.tag == "Item") {
+			return GetItemRootParent(item.parent);
+		} else {
+			return item;
+		}
+		
+	}
+}
---
+++ Assets/Scripts/Test/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	public Transform toFollow;
+	public float speed = 5f;
+	private Vector3 offset;
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - toFollow.position;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Vector3 position = transform.position;
+		transform.position = new Vector3(toFollow.position.x + offset.x, transform.position.y, toFollow.position.z + offset.z);
+	}
+}
